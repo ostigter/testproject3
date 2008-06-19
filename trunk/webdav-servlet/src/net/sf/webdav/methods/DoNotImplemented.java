1497fccc1468bdddc7c6b3f9bee13997ac032480
oscar.stigter <oscar.stigter@e0aef87a-ea4e-0410-81cd-4b1fdc67522b>
2008-06-19 12:19:38 +0000

    Added webdav-servlet subproject.

---
+++ trunk/webdav-servlet/src/net/sf/webdav/methods/DoNotImplemented.java
@@ -0,0 +1,37 @@
+package net.sf.webdav.methods;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.webdav.MethodExecutor;
+import net.sf.webdav.WebdavStatus;
+
+import org.apache.log4j.Logger;
+
+public class DoNotImplemented implements MethodExecutor {
+
+    private static Logger log =
+        Logger.getLogger("net.sf.webdav.methods");
+
+    private boolean readOnly;
+
+    public DoNotImplemented(boolean readOnly) {
+	this.readOnly = readOnly;
+    }
+
+    public void execute(HttpServletRequest req, HttpServletResponse resp)
+	    throws IOException {
+	log.debug("-- " + req.getMethod());
+
+	if (readOnly) {
+	    resp.sendError(WebdavStatus.SC_FORBIDDEN);
+
+	} else
+
+	    resp.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED);
+	// TODO implement proppatch
+
+    }
+}
