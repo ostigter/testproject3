e55d21e6c46405088e99df8107d3dde0889e6dc7
oscar.stigter <oscar.stigter@e0aef87a-ea4e-0410-81cd-4b1fdc67522b>
2008-06-20 13:09:46 +0000

    Xen: Added Util class.

---
+++ Xen/src/xen/Util.java
@@ -0,0 +1,52 @@
+package xen;
+
+
+import java.io.File;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.xml.DOMConfigurator;
+
+
+public class Util {
+    
+    
+    /** Config file for log4j. */
+    private static final String LOG_CONFIG_FILE = "log4j.xml";
+    
+    
+    private static boolean logInitialized = false; 
+    
+    
+    /**
+     * Private constructor to deny instantiation.
+     */
+    private Util() {
+        // Empty implementation.
+    }
+    
+    
+    /**
+     * Configures log4j.
+     */
+    public static void initLog4j() {
+        if (!logInitialized) {
+            File file = new File(LOG_CONFIG_FILE);
+            if (file.isFile() && file.canRead()) {
+                DOMConfigurator.configure(file.getPath());
+            } else {
+                Logger rootLogger = Logger.getRootLogger();
+                rootLogger.removeAllAppenders();
+                rootLogger.addAppender(new ConsoleAppender(
+                        new PatternLayout("%d %-5p %F:%M:%L - %m%n")));
+                rootLogger.setLevel(Level.INFO);
+            }
+            
+            logInitialized = true;
+        }
+    }
+    
+    
+}
