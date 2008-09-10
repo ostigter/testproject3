c850db0189c22a15a0f6ceb813f977d459c32a6f
oscar.stigter <oscar.stigter@e0aef87a-ea4e-0410-81cd-4b1fdc67522b>
2008-09-10 12:28:23 +0000

    Added XmlIndexer subproject.

---
+++ XmlIndexer/src/xmlindexer/XmlIndexer.java
@@ -0,0 +1,144 @@
+package xmlindexer;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+/**
+ * Indexer for XML documents using SAX and XPath-like paths.
+ * 
+ * @author Oscar Stigter
+ */
+public class XmlIndexer {
+    
+    
+    private final SAXParser parser;
+    
+    private final XPathHandler handler = new XPathHandler(); 
+    
+    
+    public XmlIndexer() {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        factory.setValidating(false);
+        factory.setXIncludeAware(false);
+        try {
+            parser = factory.newSAXParser();
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Error instantiating SAX parser: " + e.getMessage());
+        }
+    }
+    
+    
+    public void index(File file, Index[] indices) {
+        handler.setIndices(indices);
+        try {
+            parser.parse(file, handler);
+        } catch (IOException e) {
+            System.err.println(e);
+        } catch (SAXException e) {
+            System.err.println(e);
+        }
+    }
+    
+    
+    //------------------------------------------------------------------------
+    //  Inner classes
+    //------------------------------------------------------------------------
+    
+    
+    private class XPathHandler extends DefaultHandler {
+        
+        
+        private final List<String> elements;
+        
+        private Index[] indices;
+        
+        private StringBuilder sb;
+        
+        
+        public XPathHandler() {
+            elements = new ArrayList<String>();
+        }
+        
+        
+        public void setIndices(Index[] indices) {
+            this.indices = indices;
+        }
+        
+        
+        @Override
+        public void startElement(
+                String uri, String localName, String qName, Attributes attr)
+                throws SAXException {
+            System.out.println("Start Element: " + localName);
+            elements.add(localName);
+            sb = new StringBuilder();
+        }
+        
+        
+        @Override
+        public void endElement(String uri, String localName, String qName)
+                throws SAXException {
+            System.out.println("End Element:   " + localName);
+            String path = getCurrentPath();
+            System.out.println("Path: " + path);
+            for (Index index : indices) {
+                System.out.println("Index path: " + index.getPath());
+                boolean matches = false;
+                if (index.getPath().startsWith("//")) {
+                    if (path.endsWith(index.getPath())) {
+                        String text = sb.toString().trim();
+                        if (text.length() != 0) {
+                            index.setValue(text);
+                        }
+                    }
+                } else {
+                    if (path.equals(index.getPath())) {
+                        String text = sb.toString().trim();
+                        if (text.length() != 0) {
+                            index.setValue(text);
+                        }
+                    }
+                }
+                if (matches) {
+                    
+                }
+            }
+            elements.remove(elements.size() - 1);
+            sb = new StringBuilder();
+        }
+        
+
+        @Override
+        public void characters(char[] buffer, int offset, int length)
+                throws SAXException {
+            sb.append(buffer, offset, length);
+        }
+        
+        
+        private String getCurrentPath() {
+            StringBuilder sb2 = new StringBuilder();
+            for (String element : elements) {
+                sb2.append('/');
+                sb2.append(element);
+            }
+            return sb2.toString();
+        }
+        
+
+    }
+
+
+}
---
+++ XmlIndexer/test/src/xmlindexer/XmlIndexerTest.java
@@ -0,0 +1,42 @@
+package xmlindexer;
+
+
+import java.io.File;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import xmlindexer.XmlIndexer;
+
+
+public class XmlIndexerTest {
+    
+    
+    @Test
+    public void main() {
+        Index docIdIndex =
+                new Index("DocumentId", "/FooDocument/Header/Id");
+        Index docTypeIndex =
+                new Index("DocumentType", "/FooDocument/Header/Type");                 
+        Index docVersionIndex =
+                new Index("DocumentVersion", "/FooDocument/Header/Version");                 
+
+        Index[] indices = new Index[] {
+                docIdIndex,
+                docTypeIndex,
+                docVersionIndex,
+        };
+
+        XmlIndexer indexer = new XmlIndexer();
+        
+        File file = new File("test/dat/0001.xml");
+        
+        indexer.index(file, indices);
+        
+        Assert.assertEquals("0001", docIdIndex.getValue());
+        Assert.assertEquals("Foo", docTypeIndex.getValue());
+        Assert.assertEquals("v1.0", docVersionIndex.getValue());
+    }
+
+
+}
