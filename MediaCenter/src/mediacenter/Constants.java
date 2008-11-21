d21f0e7ce95aa93a3bd2814231ab05e9349b6ffe
oscar.stigter <oscar.stigter@e0aef87a-ea4e-0410-81cd-4b1fdc67522b>
2008-11-21 06:26:58 +0000

    Updated Media Center.

--- MediaCenter/src/mediacenter/Constants.java
+++ MediaCenter/src/mediacenter/Constants.java
@@ -2,7 +2,6 @@ package mediacenter;
 
 
 import java.awt.Color;
-import java.awt.Font;
 
 
 /**
@@ -14,19 +13,13 @@ public interface Constants {
 
 
     /** Background color. */
-    Color BACKGROUND = new Color(0, 100, 255);  // medium blue
+    Color BACKGROUND = new Color(0, 100, 255);  // dark blue
     
     /** Foreground color. */
-    Color FOREGROUND = Color.CYAN;
+    Color FOREGROUND = new Color(0, 200, 255);  // blue
     
     /** Selection color. */
     Color SELECTION = Color.YELLOW;
-    
-    /** Font size. */
-    int FONT_SIZE = 20;
-    
-    /** Font. */
-    Font FONT = new Font(Font.SANS_SERIF, Font.PLAIN, FONT_SIZE);
     
 
 }
--- MediaCenter/src/mediacenter/Main.java
+++ MediaCenter/src/mediacenter/Main.java
@@ -3,10 +3,8 @@ package mediacenter;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Insets;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
 
@@ -17,6 +15,7 @@ public class Main extends JFrame {
 	
 	
 	public Main() {
+	    super("Media Center");
 	    createUI();
 	}
 	
@@ -34,10 +33,22 @@ public class Main extends JFrame {
 
         GridBagConstraints gc = new GridBagConstraints();
         
-        JButton closeButton = new JButton("X");
-        closeButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                close();
+        Label titleLabel = new Label("Media Center", 500, 125);
+        gc.gridx = 0;
+        gc.gridy = 1;
+        gc.gridwidth = 2;
+        gc.gridheight = 1;
+        gc.anchor = GridBagConstraints.NORTH;
+        gc.fill = GridBagConstraints.NONE;
+        gc.weightx = 0.0;
+        gc.weighty = 0.0;
+        gc.insets = new Insets(10, 10, 10, 10);
+        getContentPane().add(titleLabel, gc);
+        
+        Button button = new Button("_", 50, 50);
+        button.addButtonListener(new ButtonListener() {
+            public void buttonClicked(Button button) {
+                minimize();
             }
         });
         gc.gridx = 0;
@@ -47,18 +58,50 @@ public class Main extends JFrame {
         gc.anchor = GridBagConstraints.NORTHEAST;
         gc.fill = GridBagConstraints.NONE;
         gc.weightx = 1.0;
-        gc.weighty = 1.0;
-        getContentPane().add(closeButton, gc);
+        gc.weighty = 0.0;
+        gc.insets = new Insets(10, 0, 0, 0);
+        getContentPane().add(button, gc);
         
-        Button button = new Button(150, 50, "Video");
+        button = new Button("X", 50, 50);
         button.addButtonListener(new ButtonListener() {
             public void buttonClicked(Button button) {
-                System.out.println(button + " clicked");
+                exit();
+            }
+        });
+        gc.gridx = 1;
+        gc.gridy = 0;
+        gc.gridwidth = 1;
+        gc.gridheight = 1;
+        gc.anchor = GridBagConstraints.NORTH;
+        gc.fill = GridBagConstraints.NONE;
+        gc.weightx = 0.0;
+        gc.weighty = 0.0;
+        gc.insets = new Insets(10, 0, 0, 10);
+        getContentPane().add(button, gc);
+        
+        button = new Button("Video", 300, 75);
+        button.addButtonListener(new ButtonListener() {
+            public void buttonClicked(Button button) {
             }
         });
         gc.gridx = 0;
         gc.gridy = 1;
-        gc.gridwidth = 1;
+        gc.gridwidth = 2;
+        gc.gridheight = 1;
+        gc.anchor = GridBagConstraints.SOUTH;
+        gc.fill = GridBagConstraints.NONE;
+        gc.weightx = 1.0;
+        gc.weighty = 1.0;
+        getContentPane().add(button, gc);
+        
+        button = new Button("Music", 300, 75);
+        button.addButtonListener(new ButtonListener() {
+            public void buttonClicked(Button button) {
+            }
+        });
+        gc.gridx = 0;
+        gc.gridy = 2;
+        gc.gridwidth = 2;
         gc.gridheight = 1;
         gc.anchor = GridBagConstraints.CENTER;
         gc.fill = GridBagConstraints.NONE;
@@ -66,14 +109,34 @@ public class Main extends JFrame {
         gc.weighty = 1.0;
         getContentPane().add(button, gc);
         
-//        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setSize(600, 400);
-        setLocationRelativeTo(null);
+        button = new Button("Pictures", 300, 75);
+        button.addButtonListener(new ButtonListener() {
+            public void buttonClicked(Button button) {
+            }
+        });
+        gc.gridx = 0;
+        gc.gridy = 3;
+        gc.gridwidth = 2;
+        gc.gridheight = 1;
+        gc.anchor = GridBagConstraints.NORTH;
+        gc.fill = GridBagConstraints.NONE;
+        gc.weightx = 1.0;
+        gc.weighty = 1.0;
+        getContentPane().add(button, gc);
+        
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+//        setSize(600, 400);
+//        setLocationRelativeTo(null);
         setVisible(true);
     }
 
 
-	private void close() {
+    private void minimize() {
+        setExtendedState(getExtendedState() | JFrame.ICONIFIED);
+    }
+
+
+	private void exit() {
 		dispose();
 	}
 
