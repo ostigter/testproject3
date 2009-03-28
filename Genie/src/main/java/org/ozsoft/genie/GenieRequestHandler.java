79981e917719e69e7c7383adb65eb3f1a6091e3a
oscar.stigter <oscar.stigter@e0aef87a-ea4e-0410-81cd-4b1fdc67522b>
2009-03-28 19:18:20 +0000

    Added Genie project.

---
+++ Genie/src/main/java/org/ozsoft/genie/GenieRequestHandler.java
@@ -0,0 +1,99 @@
+package org.ozsoft.genie;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Method;
+import java.net.Socket;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+/**
+ * A server thread handling the execution of a single service call.
+ * 
+ * After the service call has been handled, the thread is finished.
+ * 
+ * @author Oscar Stigter
+ */
+public class GenieRequestHandler extends Thread {
+	
+	private static final Logger LOG = Logger.getLogger(GenieRequestHandler.class);
+	
+	private final Socket socket;
+	private final Map<String, Object> services;
+	
+	public GenieRequestHandler(Socket socket, Map<String, Object> services) {
+		this.socket = socket;
+		this.services = services;
+		LOG.debug("Created");
+	}
+	
+	@Override
+	public void run() {
+		GenieResponse response = new GenieResponse();
+		
+		// Receive request.
+		try {
+			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+			LOG.debug("Receiving request");
+			Object obj = ois.readObject();
+			if (obj == null) {
+				response.setErrorMessage("Received null object");
+			} else if (!(obj instanceof GenieRequest)) {
+				response.setErrorMessage("Received object not a GenieRequest");
+			} else {
+				GenieRequest request = (GenieRequest) obj;
+				// Look up the service (handler).
+				String service = request.getService();
+				Object handler = services.get(service);
+				if (handler == null) {
+					response.setErrorMessage("Service not found: " + service);
+				} else {
+					// Look up the operation (method).
+					String operation = request.getOperation();
+					Method method = getOperationMethod(handler, operation);
+					if (method == null) {
+						response.setErrorMessage("Operation not found: " + operation);
+					} else {
+						// Get the arguments.
+						Object[] args = request.getArgs();
+						if (method.getParameterTypes().length != args.length) {
+							response.setErrorMessage("Invalid number of operation arguments");
+						} else {
+							// Execute the operation (calling the method).
+							try {
+								Object returnValue = method.invoke(handler, args);
+								response.setReturnValue(returnValue);
+							} catch (Exception e) {
+								response.setErrorMessage("Error invoking operation: " + e);
+							}
+						}
+					}
+				}
+			}
+
+			// Send the response.
+			LOG.debug("Sending response");
+			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+			oos.writeObject(response);
+			oos.flush();
+		} catch (Exception e) {
+			LOG.error("Error handling request", e);
+		}
+
+		LOG.debug("Finished");
+	}
+
+	private static Method getOperationMethod(
+			Object serviceHandler, String operation) {
+		for (Method method : serviceHandler.getClass().getMethods()) {
+			if (method.getName().equals(operation)) {
+				// Return first matching method (no overloading support!).
+				return method;
+			}
+		}
+		// Not found.
+		return null;
+	}
+
+}
---
+++ Genie/src/main/java/org/ozsoft/genie/GenieServer.java
@@ -0,0 +1,156 @@
+package org.ozsoft.genie;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+/**
+ * The server. 
+ * 
+ * Implemented as separate thread.
+ * 
+ * @author Oscar Stigter
+ */
+public class GenieServer implements Runnable {
+	
+	private static final Logger LOG = Logger.getLogger(GenieServer.class);
+	
+	/** Socket timeout to avoid blocked waiting for connections. */
+	private static final int SOCKET_TIMEOUT = 100;
+
+	/** The server port. */
+	private final int port;
+	
+	/** The service handlers mapped by the service name. */
+	private final Map<String, Object> services;
+	
+	/** The server thread. */
+	private Thread thread;
+	
+	/** The server socket. */
+	private ServerSocket serverSocket;
+	
+	/** Whether the server is running. */
+	private boolean isRunning = false; 
+	
+	/**
+	 * Constructor using the default server port.
+	 */
+	public GenieServer() {
+		this(GenieConstants.DEFAULT_PORT);
+	}
+	
+	/**
+	 * Constructor using a specific server port.
+	 * 
+	 * @param port
+	 *            The server port.
+	 */
+	public GenieServer(int port) {
+		this.port = port;
+		services = new HashMap<String, Object>();
+		LOG.debug("Created");
+	}
+	
+	/**
+	 * Binds a service.
+	 * 
+	 * @param name
+	 *            The service name.
+	 * @param handler
+	 *            The service handler.
+	 *            
+	 * @throws GenieException
+	 *             If a service with the specified name is already bound.
+	 */
+	public void bind(String name, Object handler) throws GenieException {
+		if (services.containsKey(name)) {
+			String msg = String.format("Service '%s' already bound", name);
+			LOG.error(msg);
+			throw new GenieException(msg);
+		}
+		services.put(name, handler);
+		LOG.info(String.format("Bound service '%s'", name));
+	}
+	
+	/**
+	 * Unbinds a service.
+	 * 
+	 * @param name
+	 *            The service name.
+	 *            
+	 * @throws GenieException
+	 *             If the service with the specified name is not bound.
+	 */
+	public void unbind(String name) throws GenieException {
+		if (!services.containsKey(name)) {
+			String msg = String.format("Service '%s 'not bound", name);
+			LOG.error(msg);
+			throw new GenieException(msg);
+		}
+		services.remove(name);
+		LOG.info(String.format("Unbound service '%s'", name));
+	}
+	
+	/**
+	 * Starts the server.
+	 * 
+	 * @throws GenieException
+	 *             If the service is already running or could not be bound to
+	 *             the port.
+	 */
+	public void start() throws GenieException {
+		if (isRunning) {
+			throw new GenieException("Server is already running");
+		}
+		
+		LOG.debug("Starting");
+		
+		try {
+			serverSocket = new ServerSocket(port);
+			serverSocket.setSoTimeout(SOCKET_TIMEOUT);
+			thread = new Thread(this, "GenieServer");
+			thread.start();
+		} catch (IOException e) {
+			String msg = String.format("Could not bind to port %d", port);
+			throw new GenieException(msg, e);
+		}
+	}
+	
+	/**
+	 * Stops the server.
+	 */
+	public void stop() {
+		if (isRunning) {
+			LOG.debug("Stopping");
+			isRunning = false;
+			thread.interrupt();
+		}
+	}
+	
+	/**
+	 * The server thread's main loop handling service calls.
+	 * Each service call is handled by a separate, multithreaded handler.
+	 */
+	public void run() {
+		isRunning = true;
+		LOG.info(String.format("Started, listening on port %d", port));
+		while (isRunning) {
+			try {
+				// Handle incoming service requests.
+				new GenieRequestHandler(
+						serverSocket.accept(), services).start();
+			} catch (SocketTimeoutException e) {
+				// Connection timeout expired; keep listening.
+			} catch (IOException e) {
+				System.err.format("Error handling request: %s", e.getMessage());
+			}
+		}
+		LOG.info("Stopped");
+	}
+
+}
