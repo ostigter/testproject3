5e7fecea25d569b44d5e03bca5f6f499712051e5
oscar.stigter <oscar.stigter@e0aef87a-ea4e-0410-81cd-4b1fdc67522b>
2009-09-23 05:24:02 +0000

    CustomUI: Cleaned code.

--- CustomUI/src/customui/Frame.java
+++ CustomUI/src/customui/Frame.java
@@ -1,50 +1,41 @@
 package customui;
 
-
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-
 public class Frame extends JPanel {
-	
-	
-	private static final long serialVersionUID = 1L;
-	
-	private final Panel panel;
-	
-	
-	public Frame() {
-		panel = new Panel();
-		initUI();
-	}
-	
-	
-	private void initUI() {
-		Label label1 = new Label("Label One");
-		Label label2 = new Label("Label Two");
-		panel.addComponent(label1);
-		panel.addComponent(label2);
-	}
-	
-	
-	public void doLayout() {
-		panel.doLayout();
-	}
-	
-	
-	@Override // JComponent
-	public void setSize(int width, int height) {
-		super.setSize(width, height);
-		panel.setWidth(width);
-		panel.setHeight(height);
-	}
-	
-	@Override // JComponent
-	protected void paintComponent(Graphics g) {
-		panel.paintComponent(g);
-	}
-	
-	
+
+    private static final long serialVersionUID = 1L;
+
+    private final Panel panel;
+
+    public Frame() {
+        panel = new Panel();
+        initUI();
+    }
+
+    private void initUI() {
+        Label label1 = new Label("Label One");
+        Label label2 = new Label("Label Two");
+        panel.addComponent(label1);
+        panel.addComponent(label2);
+    }
+
+    public void doLayout() {
+        panel.doLayout();
+    }
+
+    @Override
+    public void setSize(int width, int height) {
+        super.setSize(width, height);
+        panel.setWidth(width);
+        panel.setHeight(height);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        panel.paintComponent(g);
+    }
 
 }
--- CustomUI/src/customui/Label.java
+++ CustomUI/src/customui/Label.java
@@ -1,47 +1,38 @@
 package customui;
 
-
 import java.awt.Color;
 import java.awt.Graphics;
 
-
 public class Label extends AbstractComponent {
-	
-	
-	private String text;
-	
-	
-	public Label(String text) {
-		setText(text);
-	}
-	
-	
-	public String getText() {
-		return text;
-	}
-	
-	
-	public void setText(String text) {
-		this.text = text;
-	}
-	
-	
-	public void doLayout() {
-		// FIXME: Fixed size for testing
-		setWidth(200);
-		setHeight(50);
-	}
-	
-	
-	public void paintComponent(Graphics g) {
-		int x = getX();
-		int y = getY();
-		int width = getWidth();
-		int height = getHeight();
-		
-		g.setColor(Color.BLACK);
-		g.drawRect(x, y, width - 1, height - 1);  
-	}
 
+    private String text;
+
+    public Label(String text) {
+        setText(text);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void doLayout() {
+        // FIXME: Fixed size for testing
+        setWidth(200);
+        setHeight(50);
+    }
+
+    public void paintComponent(Graphics g) {
+        int x = getX();
+        int y = getY();
+        int width = getWidth();
+        int height = getHeight();
+
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, width - 1, height - 1);
+    }
 
 }
