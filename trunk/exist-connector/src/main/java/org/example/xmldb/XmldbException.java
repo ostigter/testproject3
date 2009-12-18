fc8340e99e8c469fe00cd06645cab91303218fdb
oscar.stigter <oscar.stigter@e0aef87a-ea4e-0410-81cd-4b1fdc67522b>
2009-12-18 06:26:54 +0000

    Added 'exist-connector' project.

---
+++ trunk/exist-connector/src/main/java/org/example/xmldb/XmldbException.java
@@ -0,0 +1,15 @@
+package org.example.xmldb;
+
+public class XmldbException extends Exception {
+    
+    private static final long serialVersionUID = 8972251689403547077L;
+
+    public XmldbException(String message) {
+	super(message);
+    }
+    
+    public XmldbException(String message, Throwable t) {
+	super(message, t);
+    }
+
+}
