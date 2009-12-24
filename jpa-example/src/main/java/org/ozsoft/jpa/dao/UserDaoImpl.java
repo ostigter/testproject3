255dba364341a93312d624e82808f0cc2a38cfe0
oscar.stigter <oscar.stigter@e0aef87a-ea4e-0410-81cd-4b1fdc67522b>
2009-12-24 14:13:26 +0000

    jpa-example: Initial import.

---
+++ jpa-example/src/main/java/org/ozsoft/jpa/dao/UserDaoImpl.java
@@ -0,0 +1,90 @@
+package org.ozsoft.jpa.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+
+import org.ozsoft.jpa.domain.User;
+
+/**
+ * User DAO implementation with JPA and JTA (local transactions).
+ * 
+ * @author Oscar Stigter
+ */
+public class UserDaoImpl implements UserDao {
+    
+    private static final String PU_NAME = "jpa-example";
+    
+    private final EntityManager em;
+    
+    /**
+     * Constructor.
+     */
+    public UserDaoImpl() {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory(PU_NAME);
+	em = emf.createEntityManager();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.ozsoft.jpa.dao.UserDao#create(org.ozsoft.jpa.domain.User)
+     */
+    @Override
+    public void create(User user) {
+	EntityTransaction tx = em.getTransaction();
+	tx.begin();
+	try {
+	    em.persist(user);
+	    tx.commit();
+	} catch (PersistenceException e) {
+	    tx.rollback();
+	    throw e;
+	}
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.ozsoft.jpa.dao.UserDao#retrieve(long)
+     */
+    @Override
+    public User retrieve(long id) {
+	return (User) em.find(User.class, id);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.ozsoft.jpa.dao.UserDao#update(org.ozsoft.jpa.domain.User)
+     */
+    @Override
+    public void update(User user) {
+	EntityTransaction tx = em.getTransaction();
+	tx.begin();
+	try {
+	    em.merge(user);
+	    tx.commit();
+	} catch (PersistenceException e) {
+	    tx.rollback();
+	    throw e;
+	}
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.ozsoft.jpa.dao.UserDao#delete(org.ozsoft.jpa.domain.User)
+     */
+    @Override
+    public void delete(User user) {
+	EntityTransaction tx = em.getTransaction();
+	tx.begin();
+	try {
+	    em.remove(user);
+	    tx.commit();
+	} catch (PersistenceException e) {
+	    tx.rollback();
+	    throw e;
+	}
+    }
+
+}
---
+++ jpa-example/src/main/java/org/ozsoft/jpa/domain/User.java
@@ -0,0 +1,47 @@
+package org.ozsoft.jpa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * User entity.
+ * 
+ * @author Oscar Stigter
+ */
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue
+    private long id;
+    
+    private String username;
+    
+    private String password;
+
+    public long getId() {
+	return id;
+    }
+
+    public void setId(long id) {
+	this.id = id;
+    }
+
+    public String getUsername() {
+	return username;
+    }
+
+    public void setUsername(String username) {
+	this.username = username;
+    }
+
+    public String getPassword() {
+	return password;
+    }
+
+    public void setPassword(String password) {
+	this.password = password;
+    }
+
+}
