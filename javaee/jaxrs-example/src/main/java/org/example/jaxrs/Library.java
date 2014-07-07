af667927fba775c5f7d84050ccf5456fe5431c4e
oscar.stigter <oscar.stigter@e0aef87a-ea4e-0410-81cd-4b1fdc67522b>
2014-07-07 18:46:01 +0000

    Created 'jaxrs-example' project.

---
+++ javaee/jaxrs-example/src/main/java/org/example/jaxrs/Library.java
@@ -0,0 +1,35 @@
+package org.example.jaxrs;
+
+import java.util.Collection;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("/library")
+public interface Library {
+
+    @GET
+    @Path("/book")
+    @Produces("application/json")
+    Collection<Book> listBooks();
+
+    @GET
+    @Path("/book/{id}")
+    @Produces("application/json")
+    Book getBook(@PathParam("id") long id);
+
+    @PUT
+    @Path("/book")
+    @Consumes("application/json")
+    void addBook(Book book);
+
+    @DELETE
+    @Path("/book/{id}")
+    void deleteBook(@PathParam("id") long id);
+
+}
