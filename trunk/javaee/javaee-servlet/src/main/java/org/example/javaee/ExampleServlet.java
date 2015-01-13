8e72609947af73c5933b31b0a68ba616f985b602
oscar.stigter <oscar.stigter@e0aef87a-ea4e-0410-81cd-4b1fdc67522b>
2015-01-13 07:32:34 +0000

    Added 'javaee-servlet' project.

---
+++ trunk/javaee/javaee-servlet/src/main/java/org/example/javaee/ExampleServlet.java
@@ -0,0 +1,21 @@
+package org.example.javaee;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ExampleServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 6381828167428989365L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        final Writer writer = response.getWriter();
+        writer.write("<h1>Example servlet</h1>\n");
+        writer.write("<p>Hello World from a simple test servlet!\n");
+    }
+}
