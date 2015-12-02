52b1dd25f2657f6360fc13b96071b3e1cd8755c1
Oscar Stigter <oscar.stigter@asml.com>
2015-12-02 15:55:47 +0100

    datatable: Enclose in JPanel

--- datatable/src/main/java/org/ozsoft/datatable/DataTable.java
+++ datatable/src/main/java/org/ozsoft/datatable/DataTable.java
@@ -1,9 +1,12 @@
 package org.ozsoft.datatable;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.RowSorter;
@@ -15,194 +18,120 @@ import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
-public class DataTable extends JTable {
+public class DataTable extends JPanel {
 
-    private static final long serialVersionUID = 5019884184139593450L;
+    private static final long serialVersionUID = 3822671047219990819L;
 
-    private DataTableModel model;
-
-    private TableRowSorter<TableModel> sorter;
+    private final Table mainTable;
 
     public DataTable() {
-        setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        mainTable = new Table();
+        JScrollPane mainScrollPane = new JScrollPane(mainTable);
+
+        setLayout(new BorderLayout());
+        add(mainScrollPane, BorderLayout.NORTH);
     }
 
     public void setColumns(List<Column> columns) {
-        model = new DataTableModel(columns);
-        super.setModel(model);
-
-        sorter = new TableRowSorter<TableModel>(model);
-        setRowSorter(sorter);
-        List<RowSorter.SortKey> sortKeys = new ArrayList<RowSorter.SortKey>();
-        sortKeys.add(new RowSorter.SortKey(0, SortOrder.ASCENDING));
-        sorter.setSortKeys(sortKeys);
-        sorter.sort();
+        mainTable.setColumns(columns);
     }
 
-    @Override
     public int getColumnCount() {
-        return (model != null) ? model.getColumnCount() : 0;
+        return mainTable.getColumnCount();
     }
 
     public Column[] getColumns() {
-        return (model != null) ? model.getColumns() : null;
+        return mainTable.getColumns();
     }
 
-    @Override
     public int getRowCount() {
-        return (model != null) ? model.getRowCount() : 0;
+        return mainTable.getRowCount();
     }
 
     public Row[] getRows() {
-        return (model != null) ? model.getRows() : null;
+        return mainTable.getRows();
     }
 
     public Object getCellValue(int rowIndex, int columnIndex) {
-        if (model == null) {
-            throw new IllegalStateException("Model not set");
-        }
-
-        int modelIndex = sorter.convertRowIndexToModel(rowIndex);
-        return model.getValueAt(modelIndex, columnIndex);
+        return mainTable.getCellValue(rowIndex, columnIndex);
     }
 
     public void setCellValue(int rowIndex, int columnIndex, Object value) {
-        if (model == null) {
-            throw new IllegalStateException("Model not set");
-        }
-
-        if (columnIndex < getColumnCount()) {
-            if (rowIndex < getRowCount()) {
-                model.getRows()[rowIndex].setCellValue(columnIndex, value);
-            } else {
-                throw new IllegalArgumentException("rowIndex out of bounds: " + rowIndex);
-            }
-        } else {
-            throw new IllegalArgumentException("columnIndex out of bounds: " + columnIndex);
-        }
-    }
-
-    @Override
-    public TableCellRenderer getCellRenderer(int rowIndex, int columnIndex) {
-        return model.getCellRenderer(rowIndex, columnIndex);
+        mainTable.setCellValue(rowIndex, columnIndex, value);
     }
 
     public void addRow(Object... cellValues) {
-        if (model == null) {
-            throw new IllegalStateException("Model not set");
-        }
-
-        model.addRow(cellValues);
+        mainTable.addRow(cellValues);
     }
 
     public void update() {
-        if (model != null) {
-            model.fireTableDataChanged();
-            resizeColumns();
-            sorter.sort();
-        }
+        mainTable.update();
     }
 
     public void clear() {
-        if (model != null) {
-            model.clear();
-        }
+        mainTable.clear();
     }
 
-    private void resizeColumns() {
-        int columnCount = getColumnCount();
-        int rowCount = getRowCount();
-        TableColumnModel columnModel = getColumnModel();
-        for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
-            TableColumn column = columnModel.getColumn(columnIndex);
-            int preferredWidth = column.getMinWidth();
-            int maxWidth = column.getMaxWidth();
+    private static class Table extends JTable {
 
-            // Header value with
-            TableCellRenderer cellRenderer = column.getHeaderRenderer();
-            if (cellRenderer == null) {
-                cellRenderer = getTableHeader().getDefaultRenderer();
-            }
-            Object headerValue = column.getHeaderValue();
-            Component comp = cellRenderer.getTableCellRendererComponent(this, headerValue, false, false, 0, columnIndex);
-            int width = comp.getPreferredSize().width + getIntercellSpacing().width + 15;
-            preferredWidth = Math.max(preferredWidth, width);
+        private static final long serialVersionUID = 5019884184139593450L;
 
-            // Row values' width
-            for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
-                cellRenderer = getCellRenderer(rowIndex, columnIndex);
-                comp = prepareRenderer(cellRenderer, rowIndex, columnIndex);
-                width = comp.getPreferredSize().width + getIntercellSpacing().width + 15;
-                preferredWidth = Math.max(preferredWidth, width);
-                if (preferredWidth >= maxWidth) {
-                    preferredWidth = maxWidth;
-                    break;
-                }
-            }
+        private DataTableModel model;
 
-            column.setPreferredWidth(preferredWidth);
+        private TableRowSorter<TableModel> sorter;
+
+        public Table() {
+            setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+            setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         }
-    }
 
-    private static class DataTableModel extends AbstractTableModel {
+        public void setColumns(List<Column> columns) {
+            model = new DataTableModel(columns);
+            super.setModel(model);
 
-        private static final long serialVersionUID = -1718244270107253916L;
-
-        private static final TableCellRenderer DEFAULT_CELL_RENDERER = new DefaultColumnRenderer();
-
-        private final List<Column> columns;
-
-        private final List<Row> rows;
-
-        public DataTableModel(List<Column> columns) {
-            if (columns == null || columns.isEmpty()) {
-                throw new IllegalArgumentException("Null or empty columns");
-            }
-
-            this.columns = columns;
-
-            rows = new ArrayList<Row>();
+            sorter = new TableRowSorter<TableModel>(model);
+            setRowSorter(sorter);
+            List<RowSorter.SortKey> sortKeys = new ArrayList<RowSorter.SortKey>();
+            sortKeys.add(new RowSorter.SortKey(0, SortOrder.ASCENDING));
+            sorter.setSortKeys(sortKeys);
+            sorter.sort();
         }
 
         @Override
         public int getColumnCount() {
-            return columns.size();
+            return (model != null) ? model.getColumnCount() : 0;
         }
 
-        @Override
-        public String getColumnName(int columnIndex) {
-            if (columnIndex < getColumnCount()) {
-                return columns.get(columnIndex).getName();
-            } else {
-                throw new IllegalArgumentException("columnIndex out of bounds: " + columnIndex);
-            }
-        }
-
-        @Override
-        public Class<?> getColumnClass(int columnIndex) {
-            if (getRowCount() > 0) {
-                Object value = getValueAt(0, columnIndex);
-                if (value != null) {
-                    return value.getClass();
-                } else {
-                    return Object.class;
-                }
-            } else {
-                return Object.class;
-            }
+        public Column[] getColumns() {
+            return (model != null) ? model.getColumns() : null;
         }
 
         @Override
         public int getRowCount() {
-            return rows.size();
+            return (model != null) ? model.getRowCount() : 0;
         }
 
-        @Override
-        public Object getValueAt(int rowIndex, int columnIndex) {
+        public Row[] getRows() {
+            return (model != null) ? model.getRows() : null;
+        }
+
+        public Object getCellValue(int rowIndex, int columnIndex) {
+            if (model == null) {
+                throw new IllegalStateException("Model not set");
+            }
+
+            int modelIndex = sorter.convertRowIndexToModel(rowIndex);
+            return model.getValueAt(modelIndex, columnIndex);
+        }
+
+        public void setCellValue(int rowIndex, int columnIndex, Object value) {
+            if (model == null) {
+                throw new IllegalStateException("Model not set");
+            }
+
             if (columnIndex < getColumnCount()) {
                 if (rowIndex < getRowCount()) {
-                    return rows.get(rowIndex).getCellValue(columnIndex);
+                    model.getRows()[rowIndex].setCellValue(columnIndex, value);
                 } else {
                     throw new IllegalArgumentException("rowIndex out of bounds: " + rowIndex);
                 }
@@ -212,40 +141,169 @@ public class DataTable extends JTable {
         }
 
         @Override
-        public boolean isCellEditable(int rowIndex, int columnIndex) {
-            return false;
-        }
-
         public TableCellRenderer getCellRenderer(int rowIndex, int columnIndex) {
-            if (columnIndex < getColumnCount()) {
-                TableCellRenderer columnRenderer = (TableCellRenderer) columns.get(columnIndex).getRenderer();
-                return (columnRenderer != null) ? columnRenderer : DEFAULT_CELL_RENDERER;
-            } else {
-                throw new IndexOutOfBoundsException("columnIndex out of bounds: " + columnIndex);
-            }
+            return model.getCellRenderer(rowIndex, columnIndex);
         }
 
         public void addRow(Object... cellValues) {
-            int columnCount = getColumnCount();
-            if (cellValues.length != columnCount) {
-                throw new IllegalArgumentException(String.format("Invalid number of columns (expected: %d, actual: %d)", columnCount,
-                        cellValues.length));
+            if (model == null) {
+                throw new IllegalStateException("Model not set");
             }
-            Row row = new Row(columnCount);
-            row.setCellValues(cellValues);
-            rows.add(row);
+
+            model.addRow(cellValues);
         }
 
-        public Column[] getColumns() {
-            return columns.toArray(new Column[0]);
-        }
-
-        public Row[] getRows() {
-            return rows.toArray(new Row[0]);
+        public void update() {
+            if (model != null) {
+                model.fireTableDataChanged();
+                resizeColumns();
+                sorter.sort();
+            }
         }
 
         public void clear() {
-            rows.clear();
+            if (model != null) {
+                model.clear();
+            }
+        }
+
+        private void resizeColumns() {
+            int columnCount = getColumnCount();
+            int rowCount = getRowCount();
+            TableColumnModel columnModel = getColumnModel();
+            for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+                TableColumn column = columnModel.getColumn(columnIndex);
+                int preferredWidth = column.getMinWidth();
+                int maxWidth = column.getMaxWidth();
+
+                // Header value with
+                TableCellRenderer cellRenderer = column.getHeaderRenderer();
+                if (cellRenderer == null) {
+                    cellRenderer = getTableHeader().getDefaultRenderer();
+                }
+                Object headerValue = column.getHeaderValue();
+                Component comp = cellRenderer.getTableCellRendererComponent(this, headerValue, false, false, 0, columnIndex);
+                int width = comp.getPreferredSize().width + getIntercellSpacing().width + 15;
+                preferredWidth = Math.max(preferredWidth, width);
+
+                // Row values' width
+                for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+                    cellRenderer = getCellRenderer(rowIndex, columnIndex);
+                    comp = prepareRenderer(cellRenderer, rowIndex, columnIndex);
+                    width = comp.getPreferredSize().width + getIntercellSpacing().width + 15;
+                    preferredWidth = Math.max(preferredWidth, width);
+                    if (preferredWidth >= maxWidth) {
+                        preferredWidth = maxWidth;
+                        break;
+                    }
+                }
+
+                column.setPreferredWidth(preferredWidth);
+            }
+        }
+
+        private static class DataTableModel extends AbstractTableModel {
+
+            private static final long serialVersionUID = -1718244270107253916L;
+
+            private static final TableCellRenderer DEFAULT_CELL_RENDERER = new DefaultColumnRenderer();
+
+            private final List<Column> columns;
+
+            private final List<Row> rows;
+
+            public DataTableModel(List<Column> columns) {
+                if (columns == null || columns.isEmpty()) {
+                    throw new IllegalArgumentException("Null or empty columns");
+                }
+
+                this.columns = columns;
+
+                rows = new ArrayList<Row>();
+            }
+
+            @Override
+            public int getColumnCount() {
+                return columns.size();
+            }
+
+            @Override
+            public String getColumnName(int columnIndex) {
+                if (columnIndex < getColumnCount()) {
+                    return columns.get(columnIndex).getName();
+                } else {
+                    throw new IllegalArgumentException("columnIndex out of bounds: " + columnIndex);
+                }
+            }
+
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                if (getRowCount() > 0) {
+                    Object value = getValueAt(0, columnIndex);
+                    if (value != null) {
+                        return value.getClass();
+                    } else {
+                        return Object.class;
+                    }
+                } else {
+                    return Object.class;
+                }
+            }
+
+            @Override
+            public int getRowCount() {
+                return rows.size();
+            }
+
+            @Override
+            public Object getValueAt(int rowIndex, int columnIndex) {
+                if (columnIndex < getColumnCount()) {
+                    if (rowIndex < getRowCount()) {
+                        return rows.get(rowIndex).getCellValue(columnIndex);
+                    } else {
+                        throw new IllegalArgumentException("rowIndex out of bounds: " + rowIndex);
+                    }
+                } else {
+                    throw new IllegalArgumentException("columnIndex out of bounds: " + columnIndex);
+                }
+            }
+
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return false;
+            }
+
+            public TableCellRenderer getCellRenderer(int rowIndex, int columnIndex) {
+                if (columnIndex < getColumnCount()) {
+                    TableCellRenderer columnRenderer = (TableCellRenderer) columns.get(columnIndex).getRenderer();
+                    return (columnRenderer != null) ? columnRenderer : DEFAULT_CELL_RENDERER;
+                } else {
+                    throw new IndexOutOfBoundsException("columnIndex out of bounds: " + columnIndex);
+                }
+            }
+
+            public void addRow(Object... cellValues) {
+                int columnCount = getColumnCount();
+                if (cellValues.length != columnCount) {
+                    throw new IllegalArgumentException(String.format("Invalid number of columns (expected: %d, actual: %d)", columnCount,
+                            cellValues.length));
+                }
+                Row row = new Row(columnCount);
+                row.setCellValues(cellValues);
+                rows.add(row);
+            }
+
+            public Column[] getColumns() {
+                return columns.toArray(new Column[0]);
+            }
+
+            public Row[] getRows() {
+                return rows.toArray(new Row[0]);
+            }
+
+            public void clear() {
+                rows.clear();
+            }
         }
     }
 }
